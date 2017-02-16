0ff0f0d79367db1a35f146f7a5ff4ed543281de6
Kharamot <jaggression@gmail.com>
2017-02-16 14:58:17 -0800

    Initial Commit

---
+++ settings2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  settings2
+//
+//  Created by Kameron Haramoto on 2/16/17.
+//  Copyright © 2017 Kameron Haramoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
